f6adc9c1b53722b7955fd4279f3905669511f447
isofinly <isofinly@protonmail.com>
2022-10-13 00:11:56 +0300

    late night code updates

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import re
+import random as r
 
 eyes = {
     0 : ":",
@@ -22,13 +23,13 @@ mouth = {
     5 : "/",
     6 : "P"
 }
-# string_set = [
-#    f'',
-#    f'',
-#    f'',
-#    f'',
-#    f''
-# ]
+results = {
+    0 : '1, 0',
+    1 : '0, 2',
+    2 : '1, 0',
+    3 : '2, 1',
+    4 : '1, 0'
+}
 
 isu = 368823
 alphabet = "\\<>{|:;8=-()OP"
@@ -43,12 +44,43 @@ string_set = [
     f"Curiosity Killed {smile_mask} The {smile} Cat {smile_mask}",
     f"Man of {smile_mask} Few Words"
 ]
+rand_string_set = [
+   f'',
+   f'',
+   f'',
+   f'',
+   f''
+]
 
+def fun_1(rand_string_set):
+    for i in range(5):
+        a = 30
+        while a > 0:
+            a -= 1
+            rand_string_set[i] += str(alphabet[r.randint(0,len(alphabet)-1)])
+    return rand_string_set
 
 def fun_2(string_set):
-   for ss in range(len(string_set)):
-      print(string_set[ss])
-      print("Total " + str(smile_mask) + " in string set:", len(re.findall(re.escape(smile_mask), string_set[ss])))
-      print("Total " + str(smile) + " in string set", len(re.findall(re.escape(smile), string_set[ss])))
+    for ss in range(len(string_set)):
+        print(string_set[ss])
+        if (len(re.findall(re.escape(smile_mask), string_set[ss]))):
+            print("• Total " + str(smile_mask) + " in string set:", len(re.findall(re.escape(smile_mask), string_set[ss])))
+        if len(re.findall(re.escape(smile), string_set[ss])):
+            print("• Total " + str(smile) + " in string set:", len(re.findall(re.escape(smile), string_set[ss])))
+        print('• Your resuts to compare with: ' + results[ss])
+        print()
 
-print(fun_2(string_set))
+def fun_3(rand_string_set):
+   fun_1(rand_string_set)
+   for ss in range(5):
+      print(rand_string_set[ss])
+      print("• Total " + str(smile_mask) + " in string set:", len(re.findall(re.escape(smile_mask), rand_string_set[ss])))
+      print()
+
+if __name__ == '__main__':
+    try:
+        print(fun_2(string_set))
+        print()
+        print(fun_3(rand_string_set))
+    except ValueError or SyntaxError:
+        print('You are doing something wrong :)')
